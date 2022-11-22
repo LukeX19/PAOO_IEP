864a5935ccb3ff17ab6fcf3d90ef2ec0cb413482
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-11-22 23:49:33 +0200

    Added Homework 2 (Laboratory5) content for third item

--- lab5/lucrarea5.cpp
+++ lab5/lucrarea5.cpp
@@ -6,31 +6,30 @@ class Car
 {
     public:
         /* Default Constructor */
-        Car() {}
+        Car() : theName("inexisting_car"), theColor("inexisting_color"), numberOfSeats(0), totalHorsePower(0) {}
 
         /* Custom Constructor */
         Car(const std::string& name, const std::string& color, const int seats, const int hp);
 
         /* Custom Copy Constructor, commented due to Item 6 */
-        Car(const Car& givenCar) :theName(givenCar.theName), theColor(givenCar.theColor), numberOfSeats(givenCar.numberOfSeats), totalHorsePower(new int(*givenCar.totalHorsePower))
+        Car(const Car& givenCar) :theName(givenCar.theName), theColor(givenCar.theColor), numberOfSeats(givenCar.numberOfSeats), totalHorsePower(givenCar.totalHorsePower)
         {}
-
-        /* Custom Assignment Operator, commented due to Item 10 */
-        /* Car& operator=(const Car& rhs)
-        {
-            this->theColor = rhs.theColor;
-            this->theName = rhs.theName;
-            this->numberOfSeats = rhs.numberOfSeats;
-            this->totalHorsePower = rhs.totalHorsePower;
-            return *this;
-        } */
 
         /* Item 11 */
         Car& operator=(const Car& rhs)
         {
-            int *pOriginalHp = totalHorsePower;
-            totalHorsePower = new int(*rhs.totalHorsePower);
-            delete pOriginalHp;
+            //int *pOriginalHp = totalHorsePower;
+            //totalHorsePower = new int(*rhs.totalHorsePower);
+            //delete pOriginalHp;
+            if(this == &rhs)
+            {
+                std::cout<<"Objects are identical!\n";
+                return *this;
+            }
+            this->theColor = rhs.theColor;
+            this->theName = rhs.theName;
+            this->numberOfSeats = rhs.numberOfSeats;
+            this->totalHorsePower = rhs.totalHorsePower;
             return *this;
         }
 
@@ -38,39 +37,90 @@ class Car
         ~Car()
         {
             /* to not have memory leaks */
-            delete totalHorsePower;
+            //delete totalHorsePower;
             std::cout<<"Object has been completely removed from memory!\n";
         }
 
         void printCar()
         {
-            std::cout<<"------------------------\nCar: " + this->theName + "\nColor: " + this->theColor + "\n" + std::to_string(this->numberOfSeats) + " seats\nHorse Power: " + std::to_string(*this->totalHorsePower) + " HP\n------------------------\n\n";
+            std::cout<<"Car: " << this->theName << "\nColor: " << this->theColor << "\n" << this->numberOfSeats << " seats\nHorse Power: " << this->totalHorsePower << " HP\n";
         }
 
     private:
         std::string theName;
         std::string theColor;
         int numberOfSeats;
-        int* totalHorsePower;
+        int totalHorsePower;
+};
+
+class Mercedes : public Car
+{
+    public:
+        Mercedes() : Car(), leatherSeats(false) {}
+
+        Mercedes(const std::string& name, const std::string& color, const int seats, const int hp, bool hasleatherseats) : Car(name, color, seats, hp), leatherSeats(hasleatherseats) {}
+
+        /* Copy Constructor */
+        Mercedes(const Mercedes& givenCar) : Car(givenCar), leatherSeats(givenCar.leatherSeats) {}
+
+        /* Assignment Operator */
+        Mercedes& operator=(const Mercedes& rhs)
+        {
+            if(this == &rhs)
+            {
+                std::cout<<"Objects are identical!\n";
+                return *this;
+            }
+            Car::operator=(rhs);
+            leatherSeats = rhs.leatherSeats;
+            return *this;
+        }
+
+        std::string getLeatherSeatsInfo()
+        {
+            return this->leatherSeats ? "Yes" : "No";
+        }
+
+    private:
+        bool leatherSeats;
 };
 
 /*Constructor for cars with only 2 doors, such as sport cars, super cars or hyper cars*/
-Car::Car(const std::string& name, const std::string& color, const int seats, const int hp) :theName(name), theColor(color), numberOfSeats(seats), totalHorsePower(new int(hp))
+Car::Car(const std::string& name, const std::string& color, const int seats, const int hp) :theName(name), theColor(color), numberOfSeats(seats), totalHorsePower(hp)
 {}
 
 int main()
 {
     Car car1("Lamborghini Urus", "Black", 4, 641);
     car1.printCar();
+    std::cout<<"\n\n\n";
 
     Car car2("Ferrari 488 GTB", "Red", 2, 660);
     car2.printCar();
+    std::cout<<"\n\n";
 
     Car car3(car1);
     car3.printCar();
+    std::cout<<"\n\n";
 
     Car car4 = car2;
     car4.printCar();
+    std::cout<<"\n\n";
 
+    Mercedes car5("Mercedes-AMG One", "Siver", 2, 1049, true);
+    car5.printCar();
+    std::cout<<"Leather seats: " << car5.getLeatherSeatsInfo();
+    std::cout<<"\n\n\n";
+
+    Mercedes car6;
+    car6 = car5;
+    car6.printCar();
+    std::cout<<"Leather seats: " << car6.getLeatherSeatsInfo();
+    std::cout<<"\n\n\n";
+
+    Mercedes car7;
+    car7.printCar();
+    std::cout<<"Leather seats: " << car7.getLeatherSeatsInfo();
+    std::cout<<"\n\n\n";
     return 0;
 }
