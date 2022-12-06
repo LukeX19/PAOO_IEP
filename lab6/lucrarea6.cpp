3c61ea1a09cdc32a2aded1f7c5afb475d4ec28cb
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-12-06 15:57:44 +0200

    Added Homework 3 (Laboratory6) content for second item

--- lab6/lucrarea6.cpp
+++ lab6/lucrarea6.cpp
@@ -2,18 +2,30 @@
 #include <string>
 #include <memory>
 
+/* Base class */
+class Uncopyable
+{
+    protected:
+        Uncopyable() {}
+        ~Uncopyable() {}
+
+    private:
+        Uncopyable(const Uncopyable&);
+        Uncopyable& operator=(const Uncopyable&);
+};
+
 /* Custom class Car which inherits the base class */
 class Car 
 {
     public:
         /* Default Constructor */
-        Car() : theName("inexisting_car"), theColor("inexisting_color"), numberOfSeats(0) {}
+        Car() : theName("inexisting_car"), theColor("inexisting_color"), numberOfSeats(0), isCarLocked(false) {}
 
         /* Custom Constructor */
         Car(const std::string& name, const std::string& color, const int seats);
 
         /* Custom Copy Constructor, commented due to Item 6 */
-        Car(const Car& givenCar) :theName(givenCar.theName), theColor(givenCar.theColor), numberOfSeats(givenCar.numberOfSeats)
+        Car(const Car& givenCar) :theName(givenCar.theName), theColor(givenCar.theColor), numberOfSeats(givenCar.numberOfSeats), isCarLocked(givenCar.isCarLocked)
         {}
 
         /* Custom assignment operator */
@@ -27,6 +39,7 @@ class Car
             this->theColor = rhs.theColor;
             this->theName = rhs.theName;
             this->numberOfSeats = rhs.numberOfSeats;
+            this->isCarLocked = rhs.isCarLocked;
             return *this;
         }
 
@@ -41,28 +54,31 @@ class Car
             std::cout<<"Car: " << this->theName << "\nColor: " << this->theColor << "\n" << this->numberOfSeats << " seats\n";
         }
 
-        // set functions for private fields
-        // void setName(const std::string& newName)
-        // {
-        //     this->theName = newName;
-        // }
-        // void setColor(const std::string& newColor)
-        // {
-        //     this->theColor = newColor;
-        // }
-        // void setNumberOfSeats(const int newNumberOfSeats)
-        // {
-        //     this->numberOfSeats = newNumberOfSeats;
-        // }
+        //let's presume this is a critical operation done on an expensive car, as there are high risks of doing it incorrectly
+        void changeCarColor(const std::string& newColor)
+        {
+            this->theColor = newColor;
+        }
+
+        bool getIsCarLocked()
+        {
+            return this->isCarLocked;
+        }
+
+        void setIsCarLocked(bool value)
+        {
+            this->isCarLocked = value;
+        }
 
     private:
         std::string theName;
         std::string theColor;
         int numberOfSeats;
+        bool isCarLocked;
 };
 
 /* Constructor */
-Car::Car(const std::string& name, const std::string& color, const int seats) :theName(name), theColor(color), numberOfSeats(seats)
+Car::Car(const std::string& name, const std::string& color, const int seats) :theName(name), theColor(color), numberOfSeats(seats), isCarLocked(false)
 {}
 
 Car* createCustomCarObject(const std::string& name, const std::string& color, const int seats)
@@ -73,11 +89,39 @@ Car* createCustomCarObject(const std::string& name, const std::string& color, co
 void printNewCustomCar(const std::string& name, const std::string& color, const int seats)
 {
     std::auto_ptr<Car> pCar(createCustomCarObject(name, color, seats));
-    // pCar -> setName(name);
-    // pCar -> setColor(color);
-    // pCar -> setNumberOfSeats(seats);
     pCar -> printCar();
 }
+
+//item14 features
+void lockObject(Car& car)
+{
+    car.setIsCarLocked(true);
+    //std::cout<<"Car is LOCKED now.\n";
+};
+
+void unlockObject(Car& car)
+{
+    car.setIsCarLocked(false);
+    //std::cout<<"Car is UNLOCKED now.\n";
+};
+
+class LockedCar: private Uncopyable
+{
+    public:
+        explicit LockedCar(Car& givenCar): pCar(givenCar)
+        {
+            //lock the car
+            lockObject(pCar);
+        }
+        ~LockedCar()
+        {
+            //unlock the car automatically by destructor
+            unlockObject(pCar);
+            std::cout<<"Object has been completely removed from memory!\n";
+        }
+    private:
+        Car& pCar;
+};
 
 int main()
 {
@@ -98,7 +142,7 @@ int main()
     std::cout<<"\n";
 
     //item 13 with "shared_ptr"
-    std::shared_ptr<Car> pCar3(createCustomCarObject("Custom Audi A4 2010", "Grey", 4));
+    std::shared_ptr<Car> pCar3(createCustomCarObject("Custom Audi A4 2010", "Silver", 4));
     pCar3 -> printCar();
     std::cout<<"\n";
 
@@ -106,5 +150,23 @@ int main()
     pCar4 -> printCar();
     std::cout<<"\n";
 
+    //item 14
+    Car specialCar("Bugatti Chiron", "Black", 2);
+    specialCar.printCar();
+    std::cout<<"Car locked: "<<specialCar.getIsCarLocked()<<"\n";
+    std::cout<<"\n";
+    //let's say we want to change the color of the Bugatti, which is a critical operation
+    //therefore, we create a block to define a critical section
+    {
+        LockedCar lc(specialCar);                   //lock the car object
+        std::cout<<"Car locked: "<<specialCar.getIsCarLocked()<<"\n";
+        
+        specialCar.changeCarColor("Red and White");          //we now change the color
+    }
+
+    std::cout<<"\n";
+    specialCar.printCar();
+    std::cout<<"Car locked: "<<specialCar.getIsCarLocked()<<"\n";
+    std::cout<<"\n";
     return 0;
 }
